f5975c463b31409700ff0035c89256b3a917023d
jonathan.calixte <jonathan.calixte@latoken.com>
2022-07-03 00:37:37 -0400

    home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,27 @@
-const Navbar =()=> {
-        return (
-            <div>
-                <h1>Navbar</h1>
-            </div>
-        );
+import Image from 'next/image'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import {useState} from 'react'
+import {GoogleLogin} from 'react-google-login'
+import {AiOutlineLogout} from 'react-icons/ai'
+import {BiSearch} from 'react-icons/bi'
+import {IoMdAdd} from 'react-icons/io'
+import Logo from '../utils/tiktik-logo.png'
+
+const Navbar = () => {
+    return (
+        <div
+            className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+            <Link href='/'>
+                <div className='w-[100px] md:w-[130px]'>
+                    <Image src={Logo}
+                           alt='over'
+                           layout='responsive'
+                           className='cursor-pointer'/>
+                </div>
+            </Link>
+        </div>
+    );
 
 }
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,45 @@
-const Sidebar =()=> {
+import {NextPage} from 'next'
+import {useRouter} from 'next/router'
+import {useState} from 'react'
+import Link from 'next/link'
+import GoogleLogin from 'react-google-login'
+import {AiOutlineMenu, AiFillHome} from 'react-icons/ai'
+import {ImCancelCircle} from 'react-icons/im'
+
+
+const Sidebar = () => {
+    const [showSidebar, setShowSidebar] = useState(true);
+    const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center' +
+        ' xl:justify-start cursor-pointer font-semibold text-[#F519997] '
     return (
         <div>
-            <h1>Sidebar</h1>
+            <div onClick={() => setShowSidebar(prev => !prev)}
+                 className='block xl:hidden m-2 ml-4 mt-3 text-xl'>
+                {
+                    showSidebar ? <ImCancelCircle/> : <AiOutlineMenu/>
+                }
+            </div>
+            {
+                showSidebar && (
+                    <div className='xl:w-400 w-20 flex flex-col
+                    justify-start mb-10 border-r-2 border-gray-100
+                    xl:border-0 p-3'>
+                        <div className='xl:border-b-2 border-gray-200
+                        xl:pd-4'>
+                            <Link href='/'>
+                                <div className={normalLink}>
+                                    <p className='text-2xl'>
+                                        <AiFillHome/>
+                                    </p>
+                                    <span className='text-xl
+                                    hidden xl:block'>for you</span>
+                                </div>
+                            </Link>
+
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 
